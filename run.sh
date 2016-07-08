b6decc0a462f2ed4eaed05f362d72a8e914a2a52
zorrofoxH <debin.huang@gmail.com>
2016-07-08 22:11:23 +0800

    for sed bugs

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ sysctl -e -p
 
 TAB=$'\t'
 sed -i "s/^${TAB}leftid=.*$/${TAB}leftid=${LEFT_PUB_IP}/g" /etc/ipsec.conf
-sed -i "s/^${TAB}leftsubnet=.*$/${TAB}leftid=${LEFT_SUB_NET}/g" /etc/ipsec.conf
-sed -i "s/^${TAB}right=.*$/${TAB}leftid=${RIGHT_PUB_IP}/g" /etc/ipsec.conf
-sed -i "s/^${TAB}rightsubnet=.*$/${TAB}leftid=${RIGHT_SUB_NET}/g" /etc/ipsec.conf
+sed -i "s/^${TAB}leftsubnet=.*$/${TAB}leftsubnet=${LEFT_SUB_NET}/g" /etc/ipsec.conf
+sed -i "s/^${TAB}right=.*$/${TAB}right=${RIGHT_PUB_IP}/g" /etc/ipsec.conf
+sed -i "s/^${TAB}rightsubnet=.*$/${TAB}rightsubnet=${RIGHT_SUB_NET}/g" /etc/ipsec.conf
 echo "%any %any : PSK \"${SECRET}\"" > /etc/ipsec.secrets
